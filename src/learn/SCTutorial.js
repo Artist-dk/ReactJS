96fa88479e6514bf23a8b234fcd2e1b40b1b0267
Artist-dk <digambarckumbhar299@gmail.com>
2024-02-06 22:05:36 +0530

    renamed file SCTutorial.scss to SCTutorial.css

--- src/learn/SCTutorial.js
+++ src/learn/SCTutorial.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import './style/SCTutorial.css';
 
 export default class SCTutorial extends Component {
   render() {
     return (
       <>
-        
+        <div className="topNav">
+
+        </div>
+        <div className="leftNav"></div>
+        <div className="rightNav"></div>
       </>
     )
   }
